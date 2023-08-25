b8852a518417d415d9a66b07254b64cbe1260177
Paras <paraschauhan0017@gmail.com>
2023-08-25 18:48:31 +0530

    checking else working APT
    APT installed
    
    Co-Authored-By: purvagpt10 <141615844+purvagpt10@users.noreply.github.com>

--- result.js
+++ result.js
@@ -3,6 +3,7 @@ function myFunction() {
   if (confirm("Please Press a button!")) {
     txt = "You pressed Button!";
   } else {
+    console.log('first')
     txt = "You pressed Cancel Button!";
   }
   document.getElementById("Test Confirm Box").innerHTML = txt;
